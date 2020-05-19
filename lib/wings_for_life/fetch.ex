3c4ac48744721ff550920be35df24a6b2476c072
kamilpikula <pikusix1@o2.pl>
2020-05-19 14:36:05 +0200

    fetch results

--- lib/wings_for_life/fetch.ex
+++ lib/wings_for_life/fetch.ex
@@ -1,9 +1,21 @@
 defmodule WingsForLife.Fetch do
+  alias WingsForLife.{Data, Progress}
 
-  @url "https://www.wingsforlifeworldrun.com/api/v1/results/2020/?page=1"
+  def fetch(actualPage \\ 1) do
+    %{body: body} = HTTPoison.get!(url(actualPage))
+    %{data: data, meta: meta} = Jason.decode!(body, keys: :atoms)
+    Data.data(data)
+    meta(meta)
+  end
 
-  def fetch do
-    %{body: body} = HTTPoison.get!(@url)
-  #  %{meta: meta} = Poison.decode!(body, keys: :atoms)
+  def url(actualPage) do
+    "https://www.wingsforlifeworldrun.com/api/v1/results/2020/?page=#{actualPage}"
+  end
+
+  def meta(meta) do
+    Progress.render(meta.currentPage, meta.lastPage)
+    unless meta.currentPage == meta.lastPage do
+      fetch(meta.currentPage + 1)
+    end
   end
 end
