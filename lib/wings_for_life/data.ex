58ad0f9219bf0671e77d2ebc0de3401d0d28a1b3
kamilpikula <pikusix1@o2.pl>
2020-05-19 14:34:18 +0200

    results as list

---
+++ lib/wings_for_life/data.ex
@@ -0,0 +1,19 @@
+defmodule WingsForLife.Data do
+  alias WingsForLife.CSV
+
+  def data(data) do
+    Enum.map(data, fn runner ->
+    [
+      runner.ranks.global.rank,
+      runner.ranks.global.gender,
+      runner.fullname,
+      runner.startnumber,
+      runner.nation,
+      runner.category,
+      runner.ranks.global.category,
+      runner.distance,
+    ] |> Enum.join(", ")
+    end)
+    |> Enum.map(&CSV.save_csv/1)
+  end
+end
